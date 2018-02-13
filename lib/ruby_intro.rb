b50d3053ec26dcdccaa4593d53606df025adccce
EC2 Default User <ec2-user@ip-172-31-45-234.us-east-2.compute.internal>
2018-02-13 18:52:25 +0000

    All test passed. 13 feb 2018

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,77 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  if arr.length > 0 then
+    arr.each do |item|
+      total += item
+    end
+    return total
+  else
+    return 0
+  end
 end
+
+#puts "sum is #{sum([])}"
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  total=0
+  if arr.length > 0 then
+    total=arr.max(2).reduce(:+)
+    return total
+  else
+    return 0
+  end
 end
 
+#puts "sum is #{max_2_sum([1,2,3])}"
+
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+ !!arr.uniq.combination(2).detect { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  source = "Hello, " + name
+  return source
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+  	return false
+  elsif (/[^AEIOU]/i =~ s[0]  && /[A-Z]/i =~ s[0])
+    return true
+  else
+    return false
+  end
 end
 
+#puts "starts_with_consonant #{starts_with_consonant?("bee")}"
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	if (s.to_s =~ /\b[01]+\b/)
+	  if (s.to_i % 4 == 0)
+		  return true
+		end
+	else
+		return false
+	end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if (isbn.empty? || price <= 0)
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    sprintf("$%.2f", @price)
+  end
 end
